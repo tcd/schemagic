5d8e477c03956753fdb632b9e79248af02246c16
Clay Dunston <claydunston@gmail.com>
2022-02-26 14:50:26 -0600

    Add `schema_url` to Config

--- lib/schemagic/config.rb
+++ lib/schemagic/config.rb
@@ -6,15 +6,20 @@ module Schemagic
     attr_accessor :env
     # @return [String] Path to folder where generated files will be written.
     attr_accessor :data_folder
+    # @return [String] Url for reference schemas.
+    attr_accessor :schema_url
 
     # @return [void]
     def initialize()
       if defined?(Rails)
         self.env = Rails.env
         self.data_folder = Rails.root.join("app", "schema")
+        self.data_folder = ""
       else
         self.env = "development"
         self.data_folder = nil
+        self.schema_url = ""
+        # self.schema_url = "https://raw.githubusercontent.com/[USERNAME]/[REPO]/master/#{config.data_folder}"
         # self.data_folder = File.join(Dir.pwd(), "schema")
       end
     end
