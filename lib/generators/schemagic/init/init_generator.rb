eb763722c016ecc8c253fc91accd491ebdb32eab
Clay Dunston <claydunston@gmail.com>
2022-02-26 12:57:52 -0600

    [WIP] initializer generator

--- lib/generators/schemagic/init/init_generator.rb
+++ lib/generators/schemagic/init/init_generator.rb
@@ -1,13 +1,15 @@
-# Generate an initializer file for Schemagic.
-class InitGenerator < Rails::Generators::Base
+module Schemagic
+  # Generate an initializer file for Schemagic.
+  class InitGenerator < Rails::Generators::Base
 
-  desc("This generator creates an initializer file at 'config/initializers/schemagic.rb' for Schemagic")
+    desc("This generator creates an initializer file at 'config/initializers/schemagic.rb' for Schemagic")
 
-  source_root(File.expand_path("templates", __dir__))
+    source_root(File.expand_path("templates", __dir__))
 
-  # @return [void]
-  def copy_initializer()
-    template("schemagic.rb.erb", "config/initializers/schemagic.rb")
+    # @return [void]
+    def copy_initializer()
+      template("schemagic.rb.erb", "config/initializers/schemagic.rb")
+    end
+
   end
-
 end
