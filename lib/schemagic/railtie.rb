0e9a24a2ff8bd348f75ca9ed26477fbbc08c108e
Clay Dunston <claydunston@gmail.com>
2022-02-26 13:41:12 -0600

    [WIP] Rake tasks

--- lib/schemagic/railtie.rb
+++ lib/schemagic/railtie.rb
@@ -1,7 +1,13 @@
 module Schemagic
   class Railtie < ::Rails::Railtie
+
+    rake_tasks do
+      load "tasks/schemagic_tasks.rake"
+    end
+
     generators do
       require "generators/schemagic/initializer/initializer_generator"
     end
+
   end
 end
--- lib/schemagic/version.rb
+++ lib/schemagic/version.rb
@@ -1,6 +1,6 @@
 module Schemagic
 
   # @return [String]
-  VERSION = "0.1.1".freeze()
+  VERSION = "0.1.2".freeze()
 
 end
