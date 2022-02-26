723a17d4c8fc9db84fcd53007973b5bf72e895ad
Clay Dunston <claydunston@gmail.com>
2022-02-26 10:27:34 -0600

    Add `Schemagic::Utility.save_to_file`. This only works inside a Rails app.

--- lib/schemagic/utility.rb
+++ lib/schemagic/utility.rb
@@ -16,5 +16,26 @@ module Schemagic
       return code_object&.tags&.filter { |x| x.tag_name == tag_name } || nil
     end
 
+    # Write out data to a JSON file in `ActiveStorage::Blob` for the current `Rails.env`.
+    # Prepends a unique timestamp to the filename.
+    #
+    # @param data [Object]
+    # @param name [String]
+    # @return [String]
+    def self.save_to_file(data, name, add_timestamp: true)
+      base_name = File.basename(name)
+      file_name = base_name
+      if add_timestamp
+        file_name = "#{Time.zone.now.to_i}.#{base_name}"
+      end
+      name.gsub!(base_name, file_name)
+      path = Rails.root.join("storage", "saved_files", name)
+      FileUtils.mkdir_p(path.parent)
+      data = JSON.pretty_generate(data) unless data.is_a?(String)
+      File.open(path, "a") { |f| f.write(data) }
+      # File.write(path, data, mode: "a")
+      return path.to_s()
+    end
+
   end
 end
